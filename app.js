45b3ea71153bc29dd779bed09e3aaf76ffba7282
Aktan Sanhal <aktansanhal@gmail.com>
2021-10-10 19:10:45 +0300

    initializing

---
+++ app.js
@@ -0,0 +1,24 @@
+document.getElementById("change").addEventListener("click",change);
+
+function change(){
+    const xhr = new XMLHttpRequest();
+
+    xhr.open("GET","http://api.exchangeratesapi.io/v1/latest?access_key=5423b321b37db81cfe3a3d9cba6fb003&format=1");
+
+    xhr.onload = function(){
+        
+
+        if(this.status === 200){
+
+            
+            const response = JSON.parse(this.responseText);
+            const rate = response.rates.TRY ;
+            const amount = Number(document.getElementById("amount").value);
+            document.getElementById("tl").value = amount * rate;
+            
+
+        }
+    }
+
+    xhr.send();
+}
